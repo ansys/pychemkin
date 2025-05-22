249883885267ff59d730f83417dd64bf376f9471
afernand <alex.fernandezluces@ansys.com>
2025-05-22 15:38:13 +0200

    doc: Add cname and add ansys-sphinx-theme options

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -7,7 +7,10 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 from datetime import datetime
+import os
 
+from ansys.chemkin import __version__
+from ansys_sphinx_theme import get_version_match
 from sphinx.builders.latex import LaTeXBuilder
 
 LaTeXBuilder.supported_image_types = ["image/png", "image/pdf", "image/svg+xml"]
@@ -15,9 +18,51 @@ LaTeXBuilder.supported_image_types = ["image/png", "image/pdf", "image/svg+xml"]
 project = "PyChemkin"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "ANSYS, Inc. <ansys.support@ansys.com>"
+cname = os.getenv("DOCUMENTATION_CNAME", default="chemkin.docs.pyansys.com")
+switcher_version = get_version_match(__version__)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+html_context = {
+    "github_user": "ansys",
+    "github_repo": "pychemkin",
+    "github_version": "main",
+    "doc_path": "doc/source",
+}
+html_theme_options = {
+    "logo": "pyansys",
+    "switcher": {
+        "json_url": f"https://{cname}/versions.json",
+        "version_match": switcher_version,
+    },
+    "check_switcher": False,
+    "github_url": "https://github.com/ansys/pychemkin",
+    "show_prev_next": False,
+    "show_breadcrumbs": True,
+    "collapse_navigation": True,
+    "use_edit_page_button": True,
+    "additional_breadcrumbs": [
+        ("PyAnsys", "https://docs.pyansys.com/"),
+    ],
+    "icon_links": [
+        {
+            "name": "Support",
+            "url": "https://github.com/ansys/pychemkin/discussions",
+            "icon": "fa fa-comment fa-fw",
+        },
+        {
+            "name": "Download documentation in PDF",
+            "url": f"https://{cname}/version/{switcher_version}/_static/assets/download/pychemkin.pdf",  # noqa: E501
+            "icon": "fa fa-file-pdf fa-fw",
+        },
+    ],
+    "ansys_sphinx_theme_autoapi": {
+        "project": project,
+    },
+}
+
 
 extensions = [
     "sphinx.ext.autodoc",
--- src/ansys/chemkin/__init__.py
+++ src/ansys/chemkin/__init__.py
@@ -26,6 +26,7 @@ core
 """
 
 from ctypes import c_int
+import importlib.metadata as importlib_metadata
 import inspect
 import os
 from pathlib import Path
@@ -72,6 +73,9 @@ from ansys.chemkin.mixture import (
     isothermal_mixing,
 )
 from ansys.chemkin.realgaseos import check_realgas_status, set_current_pressure
+
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+
 
 # show ansys (chemkin) version number
 msg = [
