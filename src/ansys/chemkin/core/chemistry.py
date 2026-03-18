fix surface species enthalpy calculation (#109)

src/ansys/chemkin/core/chemistry.py
@@ -509,7 +509,7 @@ def set_temp_array(
     temp: float,
     temp_ele: Union[float, None] = None,
     temp_ion: Union[float, None] = None,
-):
+) -> npt.NDArray[np.double]:
     """Set up the temperature array for Chemkin-CFD-API calls."""
     """
     Set up the temperature array to be used in some Chemkin-CFD-API calls.
@@ -528,7 +528,7 @@ def set_temp_array(
 
     Returns
     -------
-        t_array: 1-D c_double array of length 3
+        t_array: 1-D double array of length 3
             temperature array containing the temperatures of
             the three different types of species
     """
@@ -543,8 +543,7 @@ def set_temp_array(
     else:
         temp_i = temp_ion
     # set the temperature used in Chemkin-CFD-API calls
-    t = np.ascontiguousarray([temp, temp_e, temp_i], dtype=np.float64)
-    t_array = t.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    t_array = np.ascontiguousarray([temp, temp_e, temp_i], dtype=np.double)
     return t_array
 
 

src/ansys/chemkin/core/chemkin_wrapper.py
@@ -518,7 +518,7 @@ chemkin.KINGetAllSpeciesEnthalpy.restype = ctypes.c_int
 chemkin.KINGetAllSpeciesEnthalpy.argtypes = [
     ctypes.POINTER(ctypes.c_int),
     ctypes.POINTER(ctypes.c_int),
-    ctypes.POINTER(ctypes.c_double),
+    np.ctypeslib.ndpointer(dtype=np.double, flags="C_CONTIGUOUS"),
     np.ctypeslib.ndpointer(dtype=np.double, flags="C_CONTIGUOUS"),
 ]
 chemkin.KINGetSiteMolecularWeights.restype = ctypes.c_int
@@ -554,7 +554,7 @@ chemkin.KINGetBulkSpeciesSpecificHeat.argtypes = [
     ctypes.POINTER(ctypes.c_int),
     ctypes.POINTER(ctypes.c_int),
     ctypes.POINTER(ctypes.c_double),
-    ctypes.POINTER(ctypes.c_double),
+    np.ctypeslib.ndpointer(dtype=np.double, flags="C_CONTIGUOUS"),
     np.ctypeslib.ndpointer(dtype=np.double, flags="C_CONTIGUOUS"),
 ]
 chemkin.KINGetBulkSpeciesEntropy.restype = ctypes.c_int

tests/integration_tests/adiabaticflametemperature.py
@@ -41,6 +41,9 @@ ck.set_verbose(True)
 global interactive
 interactive = False
 
+# set numpy printing option
+np.set_printoptions(legacy="1.25")
+
 # This is a pychemkin equivalent of equil_test07
 
 # set mechanism directory (the default Chemkin mechanism data directory)
@@ -61,14 +64,14 @@ if ierror != 0:
 
 oxid = ck.Mixture(MyGasMech)
 # set mass fraction
-oxid.X = [("O2", 1.0)]
+oxid.x = [("O2", 1.0)]
 oxid.temperature = 295.15
 oxid.pressure = ck.P_ATM  # 1 atm
 # mix the fuel and the air with an air-fuel ratio of 17.19 (almost stoichiometric?
 
 fuel = ck.Mixture(MyGasMech)
 # set mole fraction
-fuel.X = [("CH4", 1.0)]
+fuel.x = [("CH4", 1.0)]
 fuel.temperature = oxid.temperature
 fuel.pressure = oxid.pressure
 